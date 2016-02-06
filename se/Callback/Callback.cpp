13bc132090197913b492739292c75d1a445577c3
cxl <cxl>
2016-02-06 09:52:36 +0000

    .se

--- se/Callback/Callback.cpp
+++ se/Callback/Callback.cpp
@@ -4,91 +4,6 @@
 
 using namespace Upp;
 
-template<class T = void(void)> class Function { };
-
-template<typename Res, typename... ArgTypes>
-class Function<Res(ArgTypes...)> : Moveable<Function<Res(ArgTypes...)>> {
-	struct WrapperBase {
-		Atomic  refcount;
-
-		virtual Res Execute(ArgTypes... args) = 0;
-		
-		WrapperBase() { refcount = 1; }
-		virtual ~WrapperBase() {}
-	};
-
-	template <class F>
-	struct Wrapper : WrapperBase {
-		F fn;
-		virtual Res Execute(ArgTypes... args) { return fn(args...); }
-
-		Wrapper(F&& fn) : fn(pick(fn)) {}
-	};
-
-	template <class F>
-	struct Wrapper2 : WrapperBase {
-		Function l;
-		F        fn;
-
-		virtual Res Execute(ArgTypes... args) { l(args...); return fn(args...); }
-
-		Wrapper2(const Function& l, F&& fn) : l(l), fn(pick(fn)) {}
-	};
-
-	WrapperBase *ptr;
-	
-	void Free() {
-		if(ptr && AtomicDec(ptr->refcount) == 0)
-			delete ptr;
-	}
-	
-	void Copy(const Function& a) {
-		ptr = a.ptr;
-		if(ptr)
-			AtomicInc(ptr->refcount);
-	}
-
-public:
-	Function() {
-		ptr = NULL;
-	}
-	
-	Function(const Function& a) {
-		Copy(a);
-	}
-	
-	Function& operator=(const Function& a) {
-		if(&a != this) {
-			Free();
-			Copy(a);
-		}
-		return *this;
-	}
-	
-	template <class F>
-	Function(F fn) {
-		ptr = new Wrapper<F>(pick(fn));
-	}
-	
-	template <class F>
-	Function& operator<<(F fn) {
-		WrapperBase *p2 = new Wrapper2<F>(*this, pick(fn));
-		Free();
-		ptr = p2;
-		return *this;
-	}
-	
-	Res operator()(ArgTypes... args) const {
-		if(ptr)
-			return ptr->Execute(args...);
-		return Res();
-	}
-
-	~Function() {
-		Free();
-	}
-};
-
 #define Callback NewCallback
 #define callback newcallback
 
@@ -104,7 +19,13 @@ Callback callback(O *object, void (M::*method)()) {
 	return [=] { (object->*method)(); };
 }
 
-/*
+template <class O, class M>
+Callback new_pteback(O *object, void (M::*method)()) {
+	Ptr<O> ptr = object;
+	return [=] { if(ptr) (ptr->*method)(); };
+}
+
+
 template <class O, class M, class Q, class T1>
 Callback callback1(O *object, void (M::*method)(Q), T1 t1)
 {
@@ -112,7 +33,6 @@ Callback callback1(O *object, void (M::*method)(Q), T1 t1)
 		(object->*method)(t1);
 	};
 }
-*/
 
 template <class P1>
 using Callback1 = Function<void (P1)>;
@@ -127,10 +47,11 @@ void Test1()
 	LOG("Test1");
 }
 
-struct Foo {
+struct Foo : Pte<Foo> {
 	int x = 0;
 	Vector<int> v;
 	
+	void Ping()                    { RLOG("Ping " << ++x); }
 	void Inc()                     { ++x; }
 	void Add(const int& m)         { x += m; }
 	void Set(const Vector<int>& s) { v = clone(s); }
@@ -157,32 +78,42 @@ CONSOLE_APP_MAIN
 	cb = callback(Test1);
 	cb();
 	
-	Foo foo;
-	RDUMP(foo.x);
-
-	cb = callback(&foo, &Foo::Inc);
-	
-	cb();
-
-	RDUMP(foo.x);
-
-	Callback1<int> cb1 = callback(&foo, &Foo::Add);
-	cb1(10);
-	RDUMP(foo.x);
-	
-//	cb = callback1(&foo, &Foo::Add, 5);
-	cb();
-	RDUMP(foo.x);
-	
 	{
-		Vector<int> a{1, 2, 3, 4, 5};
-//		cb = callback1(&foo, &Foo::Set, pick(a));
-		cb = [&, a = pick(a)] { foo.Set(a); };
+		Foo foo;
+		RDUMP(foo.x);
+	
+		cb = callback(&foo, &Foo::Inc);
+		
+		cb();
+	
+		RDUMP(foo.x);
+	
+		Callback1<int> cb1 = callback(&foo, &Foo::Add);
+		cb1(10);
+		RDUMP(foo.x);
+		
+	//	cb = callback1(&foo, &Foo::Add, 5);
+		cb();
+		RDUMP(foo.x);
+		
+		{
+			Vector<int> a{1, 2, 3, 4, 5};
+			cb = callback1(&foo, &Foo::Set, pick(a));
+	//		cb = [&, a = pick(a)] { foo.Set(a); };
+		}
+		
+		DUMP(foo.v);
+		cb();
+		DUMP(foo.v);
+		
+		cb = new_pteback(&foo, &Foo::Ping);
+		cb();
 	}
 	
-	DUMP(foo.v);
+	LOG("About to ping");
 	cb();
-	DUMP(foo.v);
+	
+	
 
 #ifndef _DEBUG
 	{
