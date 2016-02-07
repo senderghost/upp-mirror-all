425817cd4552604f2a1327609781f9ad43f9cea4
cxl <cxl>
2016-02-07 23:36:48 +0000

    .se

---
+++ se/Callbacks/build_info.h
@@ -0,0 +1,9 @@
+#define bmYEAR   2016
+#define bmMONTH  2
+#define bmDAY    8
+#define bmHOUR   0
+#define bmMINUTE 31
+#define bmSECOND 35
+#define bmTIME   Time(2016, 2, 8, 0, 31, 35)
+#define bmMACHINE "MAIN"
+#define bmUSER    "cxl"
