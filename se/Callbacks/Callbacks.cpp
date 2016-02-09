44bb084a0f7f2f5db0c28c8f876259e7d1649873
cxl <cxl>
2016-02-09 16:10:08 +0000

    .se

--- se/Callbacks/Callbacks.cpp
+++ se/Callbacks/Callbacks.cpp
@@ -2,6 +2,21 @@
 
 using namespace Upp;
 
+void Foo(const String& h)
+{
+	DDUMP(h);
+}
+
 CONSOLE_APP_MAIN
 {
+	auto x = callback(Foo);
+	x("AAA");
+	
+	auto y = callback1(x, "B");
+	y();
+	
+	Callback h = [=] { LOG("FUN"); };
+	h();
+	h = [] { LOG("FUN2"); };
+	h();
 }
--- se/Callbacks/build_info.h
+++ se/Callbacks/build_info.h
@@ -1,9 +1,9 @@
 #define bmYEAR   2016
 #define bmMONTH  2
 #define bmDAY    8
-#define bmHOUR   0
-#define bmMINUTE 31
-#define bmSECOND 35
-#define bmTIME   Time(2016, 2, 8, 0, 31, 35)
+#define bmHOUR   15
+#define bmMINUTE 17
+#define bmSECOND 11
+#define bmTIME   Time(2016, 2, 8, 15, 17, 11)
 #define bmMACHINE "MAIN"
 #define bmUSER    "cxl"
