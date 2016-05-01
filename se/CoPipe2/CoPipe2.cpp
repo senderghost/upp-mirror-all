31ffb2ba31afbc89750fb6a88331a29f3c10dcdb
cxl <cxl>
2016-05-01 11:32:47 +0000

    .se16

---
+++ se/CoPipe2/CoPipe2.cpp
@@ -0,0 +1,24 @@
+#include <Core/Core.h>
+
+using namespace Upp;
+
+Vector<String> result;
+
+void Process(CoWork& co, String h, int n)
+{
+	int x = atoi(h);
+	x++;
+	if(n < 100)
+		co.Step(n + 1, [=, &co] { Process(co, AsString(atoi(h) + 1), n + 1); });
+	else
+		result.Add(h);
+}
+
+CONSOLE_APP_MAIN
+{
+	CoWork co;
+	for(int i = 0; i < 1000; i++)
+		co.Step(0, [=, &co] { Process(co, AsString(i), 0); });
+		
+	Thread::ShutdownThreads();
+}
