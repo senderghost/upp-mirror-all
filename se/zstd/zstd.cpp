981c66fe16bb1cef1654f8b4e3757ac6f0332b09
cxl <cxl>
2016-10-21 10:49:45 +0000

    .se

--- se/zstd/zstd.cpp
+++ se/zstd/zstd.cpp
@@ -8,49 +8,31 @@ using namespace Upp;
 #include <plugin/zstd/zstd.h>
 #include <plugin/lz4/lz4.h>
 
-const char *zstd = "c:/xxx/zstd";
+#ifdef PLATFORM_POSIX
+String infn = "/home/centrum/export.log";
+String zstd = GetHomeDirFile("test.zst");
+#else
+String infn = "C:/xxx/log/local0.info";
+String zstd = "c:/xxx/test.zst";
+#endif
 
 CONSOLE_APP_MAIN
 {
 	StdLogSetup(LOG_COUT|LOG_FILE);
-
-//	String s = LoadFile("C:/u/aws.data/EastDurham_GE_1p6-100_80mHH_14WTGs_BatCurtailment_11May16.blb");
-
-#if 0
-	int csz = ZSTD_compressBound(s.GetCount());
-	
-	RDUMP(s.GetLength());
-	
-	for(int i = 1; i < 2; i++) {
-		RLOG("-------");
-		RDUMP(i);
-		Buffer<byte> c(csz);
-		TimeStop tm;
-		int len = ZSTD_compress(c, csz, s, s.GetCount(), i);
-		RDUMP(tm);
-		RDUMP(len);
-		RDUMPHEX(String(~c, 16));
-		RDUMP(ZSTD_getDecompressedSize(c, len));
-		
-		FileOut out(zstd);
-		out.Put(c, len);
-	}
-#endif
 	
 	{
 		FileOut out(zstd);
 		ZstdCompressStream zstd(out);
 //		FileIn in("C:/u/aws.data/BT2_Full.blb");
-		FileIn in("C:/xxx/log/local0.info");
+		FileIn in(infn);
 		RTIMING("Compress");
 		zstd.Put(in);
 	}
 
 	{
-		FileOut out(ForceExt(zstd, ".info"));
+		FileOut out(ForceExt(zstd, ".out"));
 		FileIn in(zstd);
 		ZstdDecompressStream zstd(in);
-//		FileIn in("C:/u/aws.data/BT2_Full.blb");
 		RTIMING("Decompress");
 		out.Put(zstd);
 	}
@@ -59,17 +41,16 @@ CONSOLE_APP_MAIN
 		FileOut out(zstd);
 		ZstdCompressStream zstd(out);
 		zstd.Concurrent();
-		FileIn in("C:/u/aws.data/BT2_Full.blb");
+		FileIn in(infn);
 		RTIMING("CoCompress");
 		zstd.Put(in);
 	}
 
 	{
-		FileOut out(ForceExt(zstd, ".info"));
+		FileOut out(ForceExt(zstd, ".out"));
 		FileIn in(zstd);
 		ZstdDecompressStream zstd(in);
 		zstd.Concurrent();
-//		FileIn in("C:/u/aws.data/BT2_Full.blb");
 		RTIMING("CoDecompress");
 		out.Put(zstd);
 	}
@@ -80,7 +61,7 @@ CONSOLE_APP_MAIN
 		FileOut out(lz4);
 		LZ4CompressStream lz4(out);
 		lz4.Concurrent();
-		FileIn in("C:/u/aws.data/BT2_Full.blb");
+		FileIn in(infn);
 		RTIMING("CoCompress LZ4");
 		lz4.Put(in);
 	}
