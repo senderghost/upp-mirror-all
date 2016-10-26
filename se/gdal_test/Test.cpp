3616b19f1f1df48999ec280193c07c6a5ebccac4
cxl <cxl>
2016-10-26 14:01:25 +0000

    .se

--- se/gdal_test/Test.cpp
+++ se/gdal_test/Test.cpp
@@ -8,18 +8,14 @@ CONSOLE_APP_MAIN
 {
 	OGRSpatialReference oSRS;
 	
-	String h = LoadFile("D:/aws.data/Ireland/OSNI_Open_Data_50K_Admin_Boundaries_–_Counties.prj");
+	String h = LoadFile("D:/aws.data/SOMERC/TA_pub_rg_rp_kantonsstrasse.prj");
+//	String h = LoadFile("D:/aws.data/Ireland/OSNI_Open_Data_50K_Admin_Boundaries_–_Counties.prj");
 	
 	StringBuffer b = h;
 	char *x = ~b;
 	DDUMP(oSRS.importFromWkt(&x));
 	
-	DDUMP(x);
 	DDUMP(oSRS.exportToProj4(&x));
-	
-	DDUMP(oSRS.importFromEPSG(29900));
-	oSRS.exportToProj4(&x);
 	DDUMP(x);
-
 }
 
