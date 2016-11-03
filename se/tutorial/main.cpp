c9a6327d3ebf159b7e8b78d12e92d80b89b57029
cxl <cxl>
2016-11-03 08:23:47 +0000

    .se

--- se/tutorial/main.cpp
+++ se/tutorial/main.cpp
@@ -12,15 +12,34 @@ GUI_APP_MAIN
 
 	TITLE("Logging");
 	
+	// Logging is a useful technique to trace the flow of the code and examine results. In this
+	// tutorial we will be using logging extensively, so let us start tutorial with the
+	// explanation of logging.
+	//
+	// In debug mode, macro LOG puts string into output log file. Log file is placed into
+	// 'config-directory', which by default is .exe directory in Win32 and ~/.upp/appname in POSIX.
+	// In TheIDE, you can access the log using 'Debug'/'View the log file Alt+L'.
+	
 	LOG("Hello world");
+	
+	// You can log values of various types, as long as they have AsString function defined
+	// (explained later). You can chain values in single LOG using operator<<:
 	
 	int x = 123;
 	LOG("Value of x is " << x);
+
+	// As it is very common to log a value of single variable, DUMP macro provides a useful shortcut,
+	// creating a log line with the variable name and value:
 	
 	DUMP(x);
 	
+	// All normal LOGs are removed in release mode. If you need to log things in release mode,
+	// you need to use RLOG / RDUMP macro instead:
+	
 	RLOG("This will also log in release mode");
 	RDUMP(x);
+	
+	// 
 	
 	DLOG("This will not compile in release mode");
 	DDUMP(x);
@@ -79,6 +98,12 @@ GUI_APP_MAIN
 	
 	CParser p("x + 123");
 	String x
+	
+	
+	
+	FindFile
+	
+	
 	
 	END
 }
