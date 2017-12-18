2fc1ae9b3a20eb1c9f52de1c2eb79e4ffff75503
cxl <cxl>
2017-12-18 23:45:15 +0000

    .uppdev

--- uppdev/CodeEditorTest/main.cpp
+++ uppdev/CodeEditorTest/main.cpp
@@ -1,15 +1,10 @@
-#include <CtrlLib/CtrlLib.h>
+#include <CodeEditor/CodeEditor.h>
 
 using namespace Upp;
 
-void ShowLen(LineEdit *edit, TopWindow *win)
-{
-	win->Title(AsString(edit->GetLength()));
-}
-
 GUI_APP_MAIN
 {
-	LineEdit edit;
+	CodeEditor edit;
 	edit.ShowSpaces();
 /*
 #ifdef _DEBUG
@@ -28,8 +23,10 @@ GUI_APP_MAIN
 		RTIMESTOP("LOAD");
 		edit.Load(in);
 	}
+	
+	edit.Highlight("log");
 
-	LoadFromFile([&](Stream& s) { edit.SerializeViewMap(s); });
+//	LoadFromFile([&](Stream& s) { edit.SerializeViewMap(s); });
 	
 	RDUMP(edit.GetLineCount());
 	RDUMP(edit.GetLength());
@@ -42,9 +39,13 @@ GUI_APP_MAIN
 	win.Open();
 	edit.ShowLineEndings();
 	edit.ShowCurrentLine(LtCyan());
-	edit <<= callback2(ShowLen, &edit, &win);
-	Ctrl::EventLoop();
 
+	edit.WhenSel = [&] {
+		win.Title(AsString(edit.GetLine(edit.GetCursor()) % 256) + " " + AsString(edit.GetLine(edit.GetCursor()) / 256));
+	};
+
+	Ctrl::EventLoop();
+	
 	StoreToFile([&](Stream& s) { edit.SerializeViewMap(s); });
 }
 
