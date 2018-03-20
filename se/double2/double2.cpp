53539e0cf5fd6c2e0d172a4739a10ec0ae9ddaaa
cxl <cxl>
2018-03-20 08:30:07 +0000

    .se

--- se/double2/double2.cpp
+++ se/double2/double2.cpp
@@ -5,7 +5,7 @@
 
 using namespace Upp;
 
-struct double2 {
+struct double2 : Moveable<double2> {
 	__m128d value;
 	
 	void SetL(double x)                   { value = _mm_loadl_pd(value, &x); }
@@ -30,9 +30,7 @@ struct double2 {
 	double2& operator/=(const double2& h) { value = _mm_div_pd(value, h.value); return *this; }
 	
 	double2 sqrt() const                  { return _mm_sqrt_pd(value); }
-	double2 abs();
-	
-	
+
 	double2(double *d2)                   { value = _mm_loadu_pd(d2); }
 	double2(double l, double h)           { SetL(l); SetH(h); }
 	double2(double lh)                    { SetLH(lh); }
@@ -48,27 +46,38 @@ double Distance(const double2& a, const double2& b)
 	return sqrt(c.GetL() + c.GetH());
 }
 
+#define N 10000000
+
 CONSOLE_APP_MAIN
 {
-	
-	RDUMP(Distance(double2(1, 2), double2(2, 3)));
-	Pointf a(1, 2);
-	Pointf b(2, 3);
-	Distance(a, b);
-	RDUMP(Distance(a, b));
-	
-	double2 x(1, 2);
-	x += 1;
-	for(int i = 0; i < 10; i++)
-		x *= 1.23;
+	Vector<double2> a;
+	Vector<double2> b;
+	Vector<double> distance;
 
-	x += { 12.0, 23.1 };
-
-	x *= 1.23;
-
-	x /= 2;
+	Vector<Pointf> pa;
+	Vector<Pointf> pb;
 	
-	x = x * 4 - 1;
+	for(int i = 0; i < N; i++) {
+		Pointf p1(Randomf(), Randomf());
+		Pointf p2(Randomf(), Randomf());
+		double d = Randomf();
+		a.Add(double2(p1.x, p1.y));
+		b.Add(double2(p2.x, p2.y));
+		distance.Add(d);
+		pa.Add(p1);
+		pb.Add(p2);
+	}
 	
-	RDUMP(x);
+	{
+		RTIMING("SSE");
+		for(int i = 0; i < a.GetCount(); i++)
+			a[i] = (a[i] - b[i]) / distance[i];
+	}
+	
+	{
+		RTIMING("FP");
+		for(int i = 0; i < a.GetCount(); i++)
+			pa[i] = (pa[i] - pb[i]) / distance[i];
+	}
+
 }
