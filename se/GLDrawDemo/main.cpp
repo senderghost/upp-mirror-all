5ce5429a7cf2437505028a5486b0a748cdfe9f4f
cxl <cxl>
2018-09-19 08:11:37 +0000

    .se

--- se/GLDrawDemo/main.cpp
+++ se/GLDrawDemo/main.cpp
@@ -4,41 +4,168 @@
 
 using namespace Upp;
 
-Image image;
+void Tesselate(Vector<Vector<Pointf>>& shape, Vector<Pointf>& vertex, Vector<Tuple<int, int, int>>& triangle);
+
+struct GLMesh {
+    unsigned int VBO, VAO, EBO;
+    int      elements;
+    
+	void Set(const float *vertices, int vcount, const int *indices, int icount);
+	void Set(const Vector<Pointf>& vertices, const Vector<int>& indices);
+    void Paint();
+
+	GLMesh();
+	~GLMesh();
+};
+
+GLMesh::GLMesh()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+}
+
+GLMesh::~GLMesh()
+{
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+}
+
+void GLMesh::Set(const float *vertices, int vcount, const int *indices, int icount)
+{
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vcount, vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * icount, indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(ATTRIB_VERTEX, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(ATTRIB_VERTEX);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+}
+
+void GLMesh::Set(const Vector<Pointf>& vertices, const Vector<int>& indices)
+{
+	Buffer<float> vx(2 * vertices.GetCount());
+	float *t = vx;
+	for(const Pointf& p : vertices) {
+		*t++ = (float)p.x;
+		*t++ = (float)p.y;
+	}
+	
+	Set(vx, vertices.GetCount() * 2, indices, indices.GetCount());
+	elements = indices.GetCount();
+}
+
+void GLMesh::Paint()
+{
+	glBindVertexArray(VAO);
+	glLineWidth(10);
+	glDrawElements(GL_LINE_LOOP, elements, GL_UNSIGNED_INT, 0);
+}
+
 
 struct OpenGLExample : GLCtrl {
 	Point point;
+	
+	OpenGLExample() {
+	}
 
 	virtual void GLPaint() {
 		Size sz = GetSize();
-/*
-		GLint total_mem_kb = 0;
-		glGetIntegerv(GL_GPU_MEMORY_INFO_TOTAL_AVAILABLE_MEMORY_NVX, &total_mem_kb);
-		DDUMP(total_mem_kb);
-*/
-//		DDUMP((char *)glGetString(GL_VENDOR));
 		
 		GLDraw w;
 		
 		w.Init(sz);
-/*
-		w.DrawRect(0, 0, sz.cx, sz.cy, SWhite);
-		w.DrawRect(10, 10, 30, 30, SRed);
-		w.DrawLine(45, 45, 80, 120, 4, Blue);
-		w.DrawLine(80, 90, 400, 0, PEN_DASHDOT);
-		w.DrawImage(200, 10, CtrlImg::reporticon());
-		const char *text = "This text is centered";
-		Size tsz = GetTextSize(text, Arial(25).Bold());
-		w.DrawText((sz.cx - tsz.cx) / 2, (sz.cy - tsz.cy) / 2, text, Arial(27).Bold(), SBlue);
-		w.Clipoff(200, 50, 95, 100);
-		w.DrawText(0, 80, "CLIPPED", Roman(25));
-		w.End();
-		
-		return;
-		// w.DrawImage(0, 0, Rescale(image, sz)); return;
-*/
-		w.Flush();
 
+		w.Flush();
+		
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+#if 1
+		static GLProgram program;
+		static GLMesh mesh;
+		ONCELOCK {
+			program.Create(R"(
+				
+
+				#version 330 core
+			    in vec4 aPos;
+				uniform mat4 u_projection;
+				uniform vec4 offset;
+				uniform vec2 scale;
+			    void main()
+			    {
+					vec4 h;
+					h.x = scale.x * aPos.x;
+					h.y = scale.y * aPos.y;
+					gl_Position = u_projection * (aPos + offset);
+			    }
+
+			)", R"(
+			
+				#version 330 core
+				out vec4 FragColor;
+				uniform vec4 ourColor;
+				void main()
+				{
+					FragColor = ourColor;
+				}
+
+				)",
+				ATTRIB_VERTEX, "aPos"
+			);
+
+			Vector<Pointf> p = { Pointf(100, 100), Pointf(200, 100), Pointf(150, 320) };
+//			Vector<Pointf> p = { Pointf(-0.3, -0.3), Pointf(0.3, -0.3), Pointf(0, 0.2) };
+			Vector<int> i = { 0, 1, 2 };
+
+			Vector<Vector<Pointf>> polygon;
+			Vector<Pointf>& c = polygon.Add();
+			for(int i = 0; i < 360; i += 20) {
+				c.Add(300 * Polar(M_2PI * i / 360.0) + Pointf(350, 350));
+				c.Add(200 * Polar(M_2PI * (i + 10) / 360.0) + Pointf(350, 350));
+			}
+		
+			Vector<Pointf> vertex;
+			Vector<Tuple<int, int, int>> triangle;
+			Tesselate(polygon, vertex, triangle);
+			
+			Vector<int> ndx;
+			for(const auto& t : triangle) {
+				ndx.Add(t.a);
+				ndx.Add(t.b);
+				ndx.Add(t.c);
+			}
+			
+			DDUMP(ndx.GetCount());
+			DDUMP(vertex.GetCount());
+
+			mesh.Set(vertex, ndx);
+
+		}
+
+		DDUMP(sz.cx);
+		DDUMP(point.x);
+
+		program.Use();
+		GLOrtho(0, (float)sz.cx, (float)sz.cy, 0, 0.0f, 1.0f, program.GetUniform("u_projection"));
+		glUniform4f(program.GetUniform("offset"), point.x, point.y, 0, 0);
+		glUniform2f(program.GetUniform("scale"), point.x / 1000.0, 1);
+		glUniform4f(program.GetUniform("ourColor"), 0.0f, (float)point.x / sz.cx, 0.0f, 1.0f);
+
+		glDisable(GL_BLEND);
+
+		mesh.Paint();
+
+#else
 		for(int i = 0; i < 200; i++) {
 			Rect r(GetSize());
 			r.Deflate(point.x + i);
@@ -76,6 +203,7 @@ struct OpenGLExample : GLCtrl {
 		
 			glDisableVertexAttribArray(ATTRIB_TEXPOS);
 		}
+#endif
 	}
 
 	virtual void MouseMove(Point p, dword) {
@@ -92,7 +220,7 @@ extern int max_texture_count;
 GUI_APP_MAIN
 {
 	max_texture_memory = 1024*1024*1024;
-	image = StreamRaster::LoadFileAny("C:/xxx/IMG_20180818_183724382.jpg");
+//	image = StreamRaster::LoadFileAny("C:/xxx/IMG_20180818_183724382.jpg");
 //	image = CtrlImg::HelpCursor1();
 //	DDUMP(image.GetSize());
 //	Ctrl::GlobalBackPaint();
---
+++ se/Tesselator/upptess.cpp
@@ -0,0 +1,73 @@
+#include <Core/Core.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "tesselator.h"
+
+using namespace Upp;
+
+void* stdAlloc(void* userData, unsigned int size)
+{
+	int* allocated = ( int*)userData;
+	return new byte[size];
+}
+
+void stdFree(void* userData, void* ptr)
+{
+	delete[] (byte *)ptr;
+}
+
+void Tesselate(Vector<Vector<Pointf>>& shape, Vector<Pointf>& vertex, Vector<Tuple<int, int, int>>& triangle)
+{
+	float t = 0.0f, pt = 0.0f;
+	TESSalloc ma;
+	TESStesselator* tess = 0;
+	unsigned char* vflags = 0;
+
+	int allocated = 0;
+	double t0 = 0, t1 = 0;
+
+	memset(&ma, 0, sizeof(ma));
+	ma.memalloc = stdAlloc;
+	ma.memfree = stdFree;
+	ma.userData = (void*)&allocated;
+	ma.extraVertices = 256; // realloc not provided, allow 256 extra vertices.
+
+	tess = tessNewTess(&ma);
+
+	tessSetOption(tess, TESS_CONSTRAINED_DELAUNAY_TRIANGULATION, 1);
+	
+	for(const auto& contour : shape) {
+		Buffer<double> data(2 * contour.GetCount());
+		double *t = ~data;
+		for(Pointf p : contour) {
+			*t++ = p.x;
+			*t++ = p.y;
+		}
+		tessAddContour(tess, 2, data, 2 * sizeof(double), contour.GetCount());
+	}
+	
+	vertex.Clear();
+	triangle.Clear();
+
+	tessTesselate(tess, TESS_WINDING_POSITIVE, TESS_POLYGONS, 3, 2, 0);
+
+	if (tess)
+	{
+		const double* verts = tessGetVertices(tess);
+		const int nverts = tessGetVertexCount(tess);
+		for(int i = 0; i < nverts; i++)
+			vertex.Add(Pointf(verts[2 * i], verts[2 * i + 1]));
+		
+		const int* elems = tessGetElements(tess);
+		const int nelems = tessGetElementCount(tess);
+
+		for(int i = 0; i < nelems; i++) {
+			const int* p = &elems[i * 3];
+			triangle.Add(MakeTuple(p[0], p[1], p[2]));
+		}
+	}
+
+	tessDeleteTess(tess);
+}
