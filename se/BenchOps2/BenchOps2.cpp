6727a9650391a62dabc606619ccce1f1e6ff57ee
cxl <cxl>
2019-02-07 10:32:04 +0000

    .se

--- se/BenchOps2/BenchOps2.cpp
+++ se/BenchOps2/BenchOps2.cpp
@@ -4,46 +4,198 @@ using namespace Upp;
 
 Vector<Tuple2<String, double>> data;
 
-int N = 10000000;
+const int N = 10000000;
 
 struct Bench {
 	String name;
 	double count;
 	int    time0;
 	
-	Bench(const char *nm, double n = N * 256) {
+	Bench(const char *nm, double n = 1) {
+		Cout() << data.GetCount() + 1 << ": " << nm << "\n";
 		name = nm;
-		time0 = msecs();
+		time0 = (double)usecs();
 		count = n;
 	}
 	
 	~Bench() {
-		RDUMP(msecs());
-		RDUMP(time0);
-		int t = msecs() - time0;
-		RDUMP(t);
+		int t = usecs() - time0;
 		data.Add(MakeTuple(name, t / count));
 	}
 };
 
-static int array[256];
-int an = 256;
+const int AN = 256;
+
+volatile int    integer[16];
+volatile int64  i64[16];
+volatile double fp64[16];
+volatile float  fp32[16];
+volatile Atomic atomic[16];
+
+struct Data64 {
+	int64 data[8];
+};
+
+Data64 data1[16], data2[16];
 
 CONSOLE_APP_MAIN
 {
 	int n = Random(128) + 1;
-	const int *end = array + an;
+	int64 n64 = n;
+	double dn = Randomf() + 0.01;
+	float fn = (float)dn;
 	{
-		Bench("Add", 1);
-		RTIMING("Add");
-		Sleep(200);
-		for(int i = 0; i < N; i++)
-			for(int *d = array; d != end; d++)
-				*d += n;
+		Bench _("Add", 100);
+		for(int i = 0; i < 100 * N; i++) {
+			integer[0] += n;
+			integer[1] += n;
+			integer[2] += n;
+			integer[3] += n;
+		}
+	}
+	{
+		Bench _("Mul", 100);
+		for(int i = 0; i < 100 * N; i++) {
+			integer[0] *= n;
+			integer[1] *= n;
+			integer[2] *= n;
+			integer[3] *= n;
+		}
+	}
+	{
+		Bench _("Div32", 100);
+		for(int i = 0; i < 100 * N; i++) {
+			integer[0] /= n;
+			integer[1] /= n;
+			integer[2] /= n;
+			integer[3] /= n;
+		}
+	}
+	{
+		Bench _("Div64", 100);
+		for(int i = 0; i < 100 * N; i++) {
+			i64[0] /= n64;
+			i64[1] /= n64;
+			i64[2] /= n64;
+			i64[3] /= n64;
+		}
+	}
+	{
+		Bench _("FP64 Add", 100);
+		for(int i = 0; i < 100 * N; i++) {
+			fp64[0] += dn;
+			fp64[1] += dn;
+			fp64[2] += dn;
+			fp64[3] += dn;
+		}
+	}
+	{
+		Bench _("FP64 Mul", 100);
+		for(int i = 0; i < 100 * N; i++) {
+			fp64[0] *= dn;
+			fp64[1] *= dn;
+			fp64[2] *= dn;
+			fp64[3] *= dn;
+		}
+	}
+	{
+		Bench _("FP64 Div", 100);
+		for(int i = 0; i < 100 * N; i++) {
+			fp64[0] /= dn;
+			fp64[1] /= dn;
+			fp64[2] /= dn;
+			fp64[3] /= dn;
+		}
+	}
+	{
+		Bench _("FP32 Add", 100);
+		for(int i = 0; i < 100 * N; i++) {
+			fp32[0] += fn;
+			fp32[1] += fn;
+			fp32[2] += fn;
+			fp32[3] += fn;
+		}
+	}
+	{
+		Bench _("FP32 Mul", 100);
+		for(int i = 0; i < 100 * N; i++) {
+			fp32[0] *= fn;
+			fp32[1] *= fn;
+			fp32[2] *= fn;
+			fp32[3] *= fn;
+		}
+	}
+	{
+		Bench _("FP32 Div", 100);
+		for(int i = 0; i < 100 * N; i++) {
+			fp32[0] /= fn;
+			fp32[1] /= fn;
+			fp32[2] /= fn;
+			fp32[3] /= fn;
+		}
+	}
+	{
+		Bench _("sin 64", 10);
+		for(int i = 0; i < 10 * N; i++) {
+			fp64[0] = sin(fp64[0]);
+			fp64[1] = sin(fp64[1]);
+			fp64[2] = sin(fp64[2]);
+			fp64[3] = sin(fp64[3]);
+		}
+	}
+	{
+		Bench _("sin 32", 10);
+		for(int i = 0; i < 10 * N; i++) {
+			fp32[0] = sin(fp32[0]);
+			fp32[1] = sin(fp32[1]);
+			fp32[2] = sin(fp32[2]);
+			fp32[3] = sin(fp32[3]);
+		}
+	}
+	{
+		Bench _("Copy 64bytes", 100);
+		for(int i = 0; i < 100 * N; i++) {
+			data1[0] = data2[0];
+			data1[1] = data2[1];
+			data1[2] = data2[2];
+			data1[3] = data2[3];
+		}
+	}
+	{
+		Bench _("AtomicInc", 10);
+		for(int i = 0; i < 10 * N; i++) {
+			AtomicInc(atomic[0]);
+			AtomicInc(atomic[1]);
+			AtomicInc(atomic[2]);
+			AtomicInc(atomic[3]);
+		}
+	}
+	{
+		Bench _("free(malloc(32))", 2);
+		for(int i = 0; i < 2 * N; i++) {
+			free(malloc(32));
+			free(malloc(32));
+			free(malloc(32));
+			free(malloc(32));
+		}
+	}
+	{
+		Bench _("delete new byte[32]", 10);
+		for(int i = 0; i < 10 * N; i++) {
+			delete new byte[32];
+			delete new byte[32];
+			delete new byte[32];
+			delete new byte[32];
+		}
+	}
+
+	RDUMP(data);
+	
+	double n0 = data[0].b;
+	for(int i = 0; i < data.GetCount(); i++) {
+		data[i].b /= n0;
+		RLOG(data[i].a << ": " << data[i].b);
 	}
 	
-	RDUMP(n);
-	RDUMP(array[0]);
-	RDUMP(array[1]);
 	RDUMP(data);
 }
