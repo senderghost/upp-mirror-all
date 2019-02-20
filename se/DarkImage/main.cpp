a88d9d71a7b4aac0fe7bf21cad082d2199ade32c
cxl <cxl>
2019-02-20 23:13:43 +0000

    .se

--- se/DarkImage/main.cpp
+++ se/DarkImage/main.cpp
@@ -3,7 +3,86 @@
 
 using namespace Upp;
 
-Color DarkTheme2(Color c)
+inline int  Gwwrayscale(int r, int g, int b) { return (77 * r + 151 * g + 28 * b) >> 8; }
+
+Color DarkTheme20(Color c)
+{
+	int h = 255 - Grayscale(c);
+
+	if(h > 128)
+		while(Grayscale(c) < h)
+			c = Color(min(c.GetR() + 1, 255), min(c.GetG() + 1, 255), min(c.GetB() + 1, 255));
+	else
+		while(Grayscale(c) > h)
+			c = Color(max(c.GetR() - 1, 0), max(c.GetG() - 1, 0), max(c.GetB() - 1, 0));
+	
+	return c;
+}
+
+Color DarkTheme2(Color color)
+{
+	if(IsNull(color))
+		return Null;
+	
+	int v[3];
+	v[0] = color.GetR();
+	v[1] = color.GetG();
+	v[2] = color.GetB();
+	
+	double c[3];
+	c[0] = 0.21;
+	c[1] = 0.72;
+	c[2] = 0.07;
+
+	int m0 = Grayscale(color);
+	int m = 256 - m0;
+	
+	int i0 = 0;
+	int i1 = 1;
+	int i2 = 2;
+	
+	if(v[i0] > v[i1])
+		Swap(i0, i1);
+	if(v[i1] > v[i2])
+		Swap(i1, i2);
+	if(v[i0] > v[i1])
+		Swap(i0, i1);
+
+	if(m0 < m) {
+		m -= m0;
+		int a = min(v[i2] + m, 255) - v[i2];
+		v[i0] += a;
+		v[i1] += a;
+		v[i2] += a;
+		m -= a;
+
+		a = min(v[i1] + int(m / (c[i0] + c[i1])), 255) - v[i1];
+		v[i0] += a;
+		v[i1] += a;
+		m -= (c[i0] + c[i1]) * a;
+
+		v[i0] = min(v[i0] + int(m / c[i1]), 255);
+	}
+	else {
+		m = m0 - m;
+		int a = v[i0] - max(v[i0] - m, 0);
+		v[i0] -= a;
+		v[i1] -= a;
+		v[i2] -= a;
+		m -= a;
+
+		a = v[i1] - max(v[i1] - int(m / (c[i1] + c[i2])), 0);
+		v[i1] -= a;
+		v[i2] -= a;
+		m -= (c[i1] + c[i2]) * a;
+
+		v[i2] = max(v[i2] - int(m / c[i2]), 0);
+	}
+	
+	return Color(v[0], v[1], v[2]);
+}
+
+Color DarkTheme3(Color c)
 {
 	int h = 255 - Grayscale(c);
 
@@ -26,13 +105,6 @@ Color DarkTheme2(Color c)
 		Swap(i1, i2);
 	if(v[i0] > v[i1])
 		Swap(i0, i1);
-
-	t[i0] = clamp(h - (v[i1] - v[i0]), 0, 255);
-	int h2 = h + (v[i2] - v[i1]);
-	t[i2] = clamp(h2, 0, 255);
-	h2 -= t[i2];
-	t[i0] -= h2;
-	t[i1] -= h2;
 	
 	return Color(t[i0], t[i1], t[i2]);
 }
@@ -89,12 +161,12 @@ struct MyApp : TopWindow {
 	virtual void Paint(Draw& w) {
 		Size sz = GetSize();
 		w.DrawRect(sz, White());
-	#if 1
+	#if 0
 		PaintImages(w, RectC(0, 0, sz.cx, sz.cy / 2), CtrlImg::Iml(), false);
 		PaintImages(w, RectC(0, sz.cy / 2, sz.cx, sz.cy / 2), CtrlImg::Iml(), true);
 	#else
 		w.DrawImage(0, 0, photo);
-		w.DrawImage(500, 0, DarkTheme(photo));
+		w.DrawImage(photo.GetSize().cx, 0, DarkTheme(photo));
 	#endif
 	}
 };
@@ -124,14 +196,16 @@ GUI_APP_MAIN
 	DDUMP(DarkTheme(c));
 
 	DLOG("====");
-	PNGEncoder().SaveFile("c:/xxx/reporticon.png", DarkTheme(CtrlImg::reporticon()));
+	PNGEncoder().SaveFile("c:/xxx/test.jpg", DarkTheme(CtrlImg::reporticon()));
 	
 	
 	
 	return;
 #endif
 
-	photo = StreamRaster::LoadFileAny("C:/xxx/IMG_20190216_112451776.png");
+//	photo = StreamRaster::LoadFileAny("C:/xxx/aukro.jpg");
+	photo = StreamRaster::LoadFileAny("C:/Users/cxl/Documents/test.jpg");
+	DDUMP(photo.GetSize());
 	
 	MyApp().Sizeable().Run();
 }
