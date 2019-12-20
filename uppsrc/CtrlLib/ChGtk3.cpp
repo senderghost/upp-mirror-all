09ba9b081f5ce631fc9fd2a55e6c88d9c93ce736
cxl <cxl>
2019-12-20 10:22:02 +0000

    gtk3: ch

--- uppsrc/CtrlLib/ChGtk3.cpp
+++ uppsrc/CtrlLib/ChGtk3.cpp
@@ -71,9 +71,122 @@ void SetupFont()
 	Font::SetDefaultFont(gui_font);
 }
 
+static GtkStyleContext *sCtx = NULL;
+static GtkStateFlags sFlags;
+
+void Gtk_Free()
+{
+	if(sCtx)
+		g_object_unref(sCtx);
+}
+
+void Gtk_New(const char *name)
+{
+	Gtk_Free();
+	sCtx = NULL;
+	for(const String& element : Split(name, ' ')) {
+		GtkWidgetPath *path = gtk_widget_path_new();
+		Vector<String> s = Split(element, '.');
+		if(s.GetCount()) {
+			gtk_widget_path_append_type (path, G_TYPE_NONE);
+			gtk_widget_path_iter_set_object_name (path, -1, s[0]);
+		}
+		for(int i = 1; i < s.GetCount(); i++)
+			gtk_widget_path_iter_add_class(path, -1, s[i]);
+
+		GtkStyleContext *context2 = gtk_style_context_new();
+		gtk_style_context_set_path(context2, path);
+		gtk_style_context_set_parent(context2, sCtx);
+		gtk_widget_path_unref(path);
+		Gtk_Free();
+		sCtx = context2;
+	}
+	ASSERT(sCtx);
+	gtk_style_context_set_scale(sCtx, DPI(1));
+}
+
+void Gtk_State(int state, dword flags = 0)
+{
+	sFlags = static_cast<GtkStateFlags>(decode(state, CTRL_HOT, GTK_STATE_FLAG_PRELIGHT,
+	                                                  CTRL_PRESSED, GTK_STATE_FLAG_ACTIVE,
+	                                                  CTRL_DISABLED, GTK_STATE_FLAG_INSENSITIVE,
+	                                                  GTK_STATE_FLAG_NORMAL) | flags) ;
+	gtk_style_context_set_state(sCtx, sFlags);
+}
+
+Color GetInkColor()
+{
+	GdkRGBA color;
+	gtk_style_context_get_color(sCtx, sFlags, &color);
+	return Color(int(255 * color.red), int(255 * color.green), int(255 * color.blue));
+}
+
+Image CairoImage(int cx, int cy, Event<cairo_t *> draw)
+{
+	Image m[2];
+	for(int i = 0; i < 2; i++) {
+		ImageDraw iw(DPI(cx), DPI(cy));
+		iw.DrawRect(0, 0, DPI(cx), DPI(cy), i ? Black() : White());
+		cairo_t *cr = iw;
+		cairo_surface_set_device_scale(cairo_get_target(cr), DPI(1), DPI(1));
+		draw(cr);
+		m[i] = iw;
+	}
+	return RecreateAlpha(m[0], m[1]);
+}
+
+void SOImages(int imli, dword flags)
+{
+	for(int st = 0; st < 4; st++) {
+		Gtk_State(st, flags);
+		CtrlsImg::Set(imli++, CairoImage(16, 16, [&](cairo_t *cr) {
+			gtk_render_check(sCtx, cr, 0, 0, 16, 16);
+		}));
+	}
+}
+
+Image CairoImage(int cx = 40, int cy = 32)
+{
+	return CairoImage(cx, cy, [&](cairo_t *cr) {
+		gtk_render_background(sCtx, cr, 0, 0, cx, cy);
+		gtk_render_frame(sCtx, cr,  0, 0, cx, cy);
+	});
+}
+
+Image Hot3(const Image& m)
+{
+	Size sz = m.GetSize();
+	return WithHotSpots(m, sz.cx / 3, sz.cy / 3, sz.cx - sz.cx / 3, sz.cy - sz.cy / 3);
+}
+
 void ChHostSkin()
 {
 	SetupFont();
+	
+	// TODO: resolve colors first
+
+	Gtk_New("radiobutton radio");
+	SOImages(CtrlsImg::I_S0, GTK_STATE_FLAG_NORMAL);
+	SOImages(CtrlsImg::I_S1, GTK_STATE_FLAG_CHECKED);
+	Gtk_New("checkbutton check");
+	SOImages(CtrlsImg::I_O0, GTK_STATE_FLAG_NORMAL);
+	SOImages(CtrlsImg::I_O1, GTK_STATE_FLAG_CHECKED);
+	SOImages(CtrlsImg::I_O2, GTK_STATE_FLAG_INCONSISTENT);
+	
+	{
+		Gtk_New("button");
+		for(int pass = 0; pass < 2; pass++) {
+			Button::Style& s = pass ? Button::StyleOk().Write() : Button::StyleNormal().Write();
+			for(int i = 0; i < 4; i++) {
+				Gtk_State(i);
+				s.look[i] = Hot3(CairoImage());
+				s.monocolor[i] = s.textcolor[i] = GetInkColor();
+			}
+			Gtk_New("button.suggested-action");
+		}
+	}
+
+	Gtk_Free();
 }
 
 Image GtkThemeIcon(const char *name, int rsz)
