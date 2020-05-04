5cd2557c9caf7dde8148dc4cce244608964c0043
cxl <cxl>
2020-05-04 09:23:26 +0000

    .se

--- se/ProcessList/ProcessList.cpp
+++ se/ProcessList/ProcessList.cpp
@@ -1,7 +1,9 @@
 #include <Core/Core.h>
-#include <tlhelp32.h>
 
 using namespace Upp;
+
+#ifdef PLATFORM_WIN32
+#include <tlhelp32.h>
 
 VectorMap<String, int> GetProcessList()
 {
@@ -18,8 +20,42 @@ VectorMap<String, int> GetProcessList()
 	}
 	return pl;
 }
+#endif
+
+#ifdef PLATFORM_POSIX
+
+VectorMap<String, int> GetProcessList()
+{
+	VectorMap<String, int> pl;
+	for(FindFile ff("/proc/*"); ff; ff.Next()) {
+		String n = ff.GetName();
+		if(IsDigit(*n)) {
+			int pid = atoi(n);
+			static char h[_MAX_PATH + 1];
+			int ret = readlink(ff.GetPath() +
+			#ifdef PLATFORM_BSD
+				"/file"
+			#else
+				"/exe"
+			#endif
+			     , h, _MAX_PATH);
+			if(ret > 0 && ret < _MAX_PATH) {
+				h[ret] = '\0';
+				pl.Add(h, pid);
+			}
+		}
+	}
+	return pl;
+}
+
+#endif
 
 CONSOLE_APP_MAIN
 {
 	DDUMPM(GetProcessList());
+	DLOG("============");
+	VectorMap<String, int> m = GetProcessList();
+	for(int i = 0; i < m.GetCount(); i++)
+		if(GetFileTitle(m.GetKey(i)) == "theide")
+			DDUMP(m[i]);
 }
