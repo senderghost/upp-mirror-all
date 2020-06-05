7029063fab4903bfb90dc5681fd20155c775cbb1
cxl <cxl>
2020-06-05 14:49:00 +0000

    .se

--- se/MemoryStatus/MemoryStatus.cpp
+++ se/MemoryStatus/MemoryStatus.cpp
@@ -4,6 +4,12 @@ using namespace Upp;
 
 CONSOLE_APP_MAIN
 {
+
+#ifdef PLATFORM_POSIX
+	int pgsz = getpagesize();
+	DDUMP(sysconf (_SC_PHYS_PAGES) * pgsz >> 20);
+	DDUMP(sysconf (_SC_AVPHYS_PAGES) * pgsz >> 20);
+#else
   MEMORYSTATUSEX statex;
 
   statex.dwLength = sizeof (statex);
@@ -13,5 +19,6 @@ CONSOLE_APP_MAIN
 	DDUMP(statex.dwMemoryLoad);
 	DDUMP(statex.ullTotalPhys);
 	DDUMP(statex.ullAvailPhys);
+#endif
 }
 
