ddefb1320b477d8d0e6a7ebe5701fba2c119a7df
cxl <cxl>
2020-07-29 10:07:14 +0000

    .uppdev

--- uppdev/RichEditTest/main.cpp
+++ uppdev/RichEditTest/main.cpp
@@ -14,6 +14,19 @@ GUI_APP_MAIN
 	Ctrl::SetUHDEnabled();
 
 	RichEditWithToolBar e;
+	PaintInfo pi;
+	pi.DrawSelection = [](Draw& dw, int x, int y, int cx, int cy) {
+		static Image m;
+		ONCELOCK {
+			m = CreateImage(Size(128, 128), 50 * Blue());
+		}
+		dw.Clip(x, y, cx, cy);
+		for(int w = 0; w < cx; w += 128)
+			for(int h = 0; h < cy; h += 128)
+				dw.DrawImage(x + w, y + h, m);
+		dw.End();
+	};
+	e.SetPaintInfo(pi);
 	e.Pick(ParseQTF(LoadFile(FileName())));
 //	e.SetPage(Size(4000, 1600));
 	TopWindow w;
